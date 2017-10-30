c6df143edca9b89b8c7536dc05fe4f3e2fa6b1be
Alexandra Onegård <onegard@kth.se>
2017-10-30 09:47:52 +0100

    kan välja mellan default och alternative input

--- runProgram.js
+++ runProgram.js
@@ -1,4 +1,8 @@
 function runProgram() {
+	var alternative = $('input[name="inputRb"]:checked').val();
+	alert("alt=" + alternative);
+	
+	if (alternative == "def") {
 	$.ajax({
 		type: "POST",
 		url: 'runProgram.php',
@@ -7,9 +11,7 @@ function runProgram() {
 			param: 'Input/cyano6.rtree Input/cyano.stree',
 			guesttree: document.getElementById("guest").files[0].name,
 			arttree: document.getElementById("art").files[0].name
-//			mapfile: document.getElementById("mapfile").files[0].name
 		},
-		// Här ska datat från programmet returneras (bilden)
 		success: function(html) {
 			//alert(html);
 			location.reload();
@@ -19,7 +21,29 @@ function runProgram() {
         alert(xhr.status);
         alert(thrownError);
     }
-
+	});
+	 } 
+	else {
+		$.ajax({
+			type: "POST",
+			url: 'runProgram.php',
+			data: {
+				action: 'call_this',
+				param: 'Input/cyano6.rtree Input/cyano.stree',
+				guesttree: document.getElementById("guest").files[0].name,
+				arttree: document.getElementById("art").files[0].name,
+				mapfile: document.getElementById("mapfile").files[0].name
+			},
+			success: function(html) {
+			//alert(html);
+			location.reload();
+		},
+		  // Alert status code and error if fail
+    error: function (xhr, ajaxOptions, thrownError){
+        alert(xhr.status);
+        alert(thrownError);
+    }
 
 	});
+}
 }
